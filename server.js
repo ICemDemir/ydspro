df1ec3a5485525ebe66f3385f2b113e61709d60e
ICemDemir <ismailcemdemir@gmail.com>
2024-12-11 23:56:42 +0300

    try

--- server.js
+++ server.js
@@ -5,8 +5,22 @@ dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
 
-const port = process.env.PORT || 8000;
+const DB = process.env.DATABASE.replace(
+  "<db_password>",
+  process.env.DB_PASSWORD
+);
 
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connection successful"))
+  .catch((err) => {
+    console.error("DB connection error", err.message);
+  });
+
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
   console.log(process.env.NODE_ENV);
